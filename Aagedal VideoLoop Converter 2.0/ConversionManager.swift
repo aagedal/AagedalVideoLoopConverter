76039ddac443c6548baaac8b88595733d3a28526
Truls Aagedal <rtxryz@gmail.com>
2024-07-02 17:57:24 +0200

    Initial commit

---
+++ Aagedal VideoLoop Converter 2.0/ConversionManager.swift
@@ -0,0 +1,8 @@
+//
+//  ConversionManager.swift
+//  Aagedal VideoLoop Converter 2.0
+//
+//  Created by Truls Aagedal on 02/07/2024.
+//
+
+import Foundation
---
+++ Aagedal VideoLoop Converter 2.0/VideoFileListView.swift
@@ -0,0 +1,8 @@
+//
+//  VideoFileListView.swift.swift
+//  Aagedal VideoLoop Converter 2.0
+//
+//  Created by Truls Aagedal on 02/07/2024.
+//
+
+import Foundation
